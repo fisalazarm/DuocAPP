2aa28da9c45a530497ee0426b207d08c61510c8e
fisalazarm <fabian.salazar.mallea@gmail.com>
2021-03-12 16:56:18 -0300

    commit

--- index.php
+++ index.php
@@ -4,6 +4,8 @@
 <?php include('head.php'); ?>
 <?php include('nav-bar.php'); ?>
 <body>
-    
+    <div class="container-md d-flex justify-content-center" style="background-color:white" text-center py-5>
+        <a href="responderEncuesta.php"><button type="button" id="Enviar"  > Responder Encuesta</button></a>
+    </div> 
 </body>
 </html>
--- nav-bar.php
+++ nav-bar.php
@@ -2,7 +2,7 @@
 <!DOCTYPE html>
 <html>
     <header>
-        <div class="navbar navbar-dark shadow-sm" id="barra" py-3>
+        <div class="navbar-lg navbar-dark shadow-sm" id="barra" py-3>
             <div class="navbar d-flex justify-content-md-between">
                 <a href="#" id="navbar" class="navbar-brand d-flex">
                     <img class=".img-fluid. max-width: 100%; height: auto;" id="LogoDuoc" src="images/logo amarillo-blanco.png" alt="LogoDuocAmarilloBlanco">
