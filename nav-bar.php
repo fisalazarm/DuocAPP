4de1c0d7ea1dfbb13ebbddb53f7e6101c68dac41
fisalazarm <fabian.salazar.mallea@gmail.com>
2021-03-14 17:25:17 -0300

    navbar con imagen responsiva

--- nav-bar.php
+++ nav-bar.php
@@ -4,7 +4,7 @@
     <header>
         <div class="bs-example">
             <nav class="navbar navbar-expand-md navbar-dark shadow-sm" id="barra">
-                <a href="responderEncuesta.php"><img class=".img-fluid. max-width: 100%; height: 28;" id="LogoDuoc" src="images/logo amarillo-blanco.png" alt="LogoDuocAmarilloBlanco"></a>   
+                <a href="index.php" id="LogoDuoc"><img class=".img-fluid. max-width: 100%; height: 28;" src="images/logo amarillo-blanco.png" alt="LogoDuocAmarilloBlanco"></a>   
                 <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
                     <span class="navbar-toggler-icon"></span>
                 </button>
