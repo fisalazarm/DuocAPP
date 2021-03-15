a2309b6f841906e8920d39cf81a93f968ae1ed91
felipetorresf <felipeantonio.torresfarias@gmail.com>
2021-03-15 17:03:23 -0300

    Inicio de sesión

--- Reportes.php
+++ Reportes.php
@@ -1,9 +1,16 @@
 <html>
-<?php $title = 'Reportes'; ?>
+<?php $title = 'Administrador'; ?>
 <?php $currentPage = 'Reportes'; ?>
 <?php include('head.php'); ?>
 <?php include('nav-bar.php'); ?>
-<body>
 
-</body>
+<body>
+<?php
+    session_start();
+    $usuario = $_SESSION['username'];
+    echo  "<h1>Bienvenido $usuario al sistema de reportes</h1>";
+
+ 
+?>
+</body>    
 </html>
