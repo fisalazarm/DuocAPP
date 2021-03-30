5c2d81abad5f11e3da3e5a0107a02f710e82b27e
felipetorresf <felipeantonio.torresfarias@gmail.com>
2021-03-30 17:02:17 -0400

    encuestaV3

--- crearCuestionario.php
+++ crearCuestionario.php
@@ -6,34 +6,39 @@
     <?php include('conexion.php'); ?>
     <?php
 error_reporting(E_ALL ^ E_NOTICE);
+    session_start();
+    $usuario = $_SESSION['username'];
 
     if(isset($_POST["pregunta1"])){
-        $asignatura = =$_POST["asignatura"];
+        $asignatura = $_POST["asignatura"];
         $pregAgre1 = $_POST["pregunta1"];
         $pregAgre2 = $_POST["pregunta2"];
         $pregAgre3 = $_POST["pregunta3"];
-        $clase = $_POST["codigo"];
+        $seccion = $_POST["codseccion"];
+        $usu = $usuario;
 
         $numRandom ="SELECT LPAD(FLOOR(RAND() * 999999.99), 6, '0') FROM DUAL";
-        $insertar ="INSERT INTO encuesta (codigo_encuesta,id_clasefk,pregunta1,pregunta2,pregunta3,dia_encuesta,hora_encuesta,id_profesorfk) 
-        VALUES (FLOOR(1+ RAND() * 999999),1,'$pregAgre1','$pregAgre2','$pregAgre3',curdate(),curtime(),2)";
+        $insertar ="INSERT INTO encuesta (codigo_encuesta,id_clasefk,seccion,pregunta1,pregunta2,pregunta3,dia_encuesta,hora_encuesta,id_profesorfk) 
+        VALUES (FLOOR(1+ RAND() * 999999),'$asignatura','$seccion','$pregAgre1','$pregAgre2','$pregAgre3',curdate(),curtime(),'$usuario')";
         
         $con->query($insertar);
-      /*  if($con->query($insertar) === true){
-            echo '<div><form action=""><input type="checkbox">'.$pregAgre.'</form></div>';
-        }else{
-            die("Error al insertar datos: " . $con->error);
-        }*/ 
+        
    
     }
     
 ?>
+<?php
+    
+  
+
+?>
+
     <body>
         <div class="container-md d-flex justify-content-center" style="background-color:white" text-center py-5>
             <form action="crearCuestionario.php" method="POST" id="form" class="form-md">
             <td>
             <td>Sección:</td>
-            <input type="text" placeholder="Indique su sección">
+            <input type="text" name="codseccion" placeholder="Indique su sección" required>
             </td>
             <br>
             <br>
@@ -44,7 +49,7 @@ error_reporting(E_ALL ^ E_NOTICE);
                                 $result = $con->query("select * FROM clases");
                                 while ($row = $result->fetch_assoc())
                                     { 
-                                        echo "<option required value='".$row['nombre_clase']."'>".$row['nombre_clase']." </option>";
+                                        echo "<option required value='".$row['id_clase']."'>".$row['nombre_clase']." </option>";
                                     }
                                     
                             ?>
