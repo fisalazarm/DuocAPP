52832c90a494b980292323a220d013d81a728712
felipetorresf <felipeantonio.torresfarias@gmail.com>
2021-04-06 11:21:13 -0400

    sabanaV1

--- ResultadoEncuestaG.php
+++ ResultadoEncuestaG.php
@@ -22,6 +22,7 @@
                     <th>Respuesta 2</th>
                     <th>Pregunta 3</th>
                     <th>Respuesta 3</th>
+                    <th>Comentario</th>
                 </thead>
                 <tbody>
                     <?php
@@ -29,7 +30,8 @@
                         tiu.descripcion_usuario as us,CONCAT( en.dia_encuesta,' ', en.hora_encuesta) AS hf,
                         en.pregunta1 as p1, re.respuesta1 as r1,
                            en.pregunta2 as p2, re.respuesta2 as r2,
-                           en.pregunta3 as p3, re.respuesta3 as r3
+                           en.pregunta3 as p3, re.respuesta3 as r3,
+                           re.comentario as com
                 FROM encuesta en
                             INNER JOIN respuesta_encuesta re on en.codigo_encuesta = re.id_encuestafk
                            INNER JOIN usertable ut on ut.id = en.id_profesorfk
@@ -50,6 +52,8 @@
                         <td><?php echo $sabana['r2']?></td>
                         <td><?php echo $sabana['p2']?></td>
                         <td><?php echo $sabana['r3']?></td>
+                        <td><?php echo $sabana['com']?></td>
+
                     </tr>
                     <?php
                         }
