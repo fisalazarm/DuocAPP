342f57ff3dd964534ada83d3081b8091ab031580
fisalazarm <fabian.salazar.mallea@gmail.com>
2021-04-13 10:38:40 -0400

    nuevas vistas

---
+++ IngresoUsuarios.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<?php $title = 'Carga Masiva de usuarios'; ?>
+<?php $currentPage = 'Carga masiva de usuarios'; ?>
+<?php include('head.php'); ?>
+<?php include('nav-bar.php'); ?>
+<?php include('conexion.php'); ?>
+<html lang="es">
+	<body>
+ 
+	<div id="wrap">
+	<div class="container">
+		<div class="row">
+			<div class="span3 hidden-phone"></div>
+			<div class="span6" id="form-login">
+				<form class="form-horizontal well" action="insertarUsuarios.php" method="post" name="upload_excel" enctype="multipart/form-data">
+					<fieldset class="container-md d-flex justify-content-center">
+						<legend>Importar CSV/Excel Archivo</legend>
+						<div class="control-group">
+							<div class="control-label">
+								<label>Archivo CSV/Excel:</label>
+							</div>
+							<div class="controls">
+								<input type="file" name="file" id="file" class="input-large">
+							</div>
+                            <br>
+						</div>
+ 
+						<div class="control-group ">
+							<div class="controls">
+							<button type="submit" id="submit" name="Import" class="btn btn-primary button-loading" data-loading-text="Loading...">Subir Archivo</button>
+							</div>
+						</div>
+                        <br>
+					</fieldset>
+				</form>
+			</div>
+			<div class="span3 hidden-phone"></div>
+		</div>
+ 
+		<table class="table table-bordered">
+			<thead>
+				  	<tr>
+				  		<th>Nombres</th>
+				  		<th>Apellidos</th>
+				  		<th>Usuario</th>
+				  		<th>Email</th>
+				  		<th>Contraseña</th>
+                        <th>Tipo_Usuario</th>
+				  	</tr>	
+				  </thead>
+			<?php
+				$SQLSELECT = "SELECT nombres,apellidos,username,email,password,tipo_usuario FROM usertable ";
+				$result_set =  mysqli_query($con, $SQLSELECT);
+				while($row = mysqli_fetch_array($result_set))
+				{
+				?>
+ 
+					<tr>
+						<td><?php echo $row['nombres']; ?></td>
+						<td><?php echo $row['apellidos']; ?></td>
+						<td><?php echo $row['username']; ?></td>
+						<td><?php echo $row['email']; ?></td>
+						<td><?php echo $row['password']; ?></td>
+                        <td><?php echo $row['tipo_usuario']; ?></td> 
+					</tr>
+				<?php
+				}
+			?>
+		</table>
+	</div>
+ 
+	</div>
+ 
+	</body>
+</html>
---
+++ insertarUsuario.php
@@ -0,0 +1,48 @@
+<?php
+include 'conexion.php';
+set_time_limit(300);
+if(isset($_POST["Import"])){
+ 
+ 
+		echo $filename=$_FILES["file"]["tmp_name"];
+ 
+ 
+		 if($_FILES["file"]["size"] > 0)
+		 {
+ 
+		  	$file = fopen($filename, "r");
+	         while (($emapData = fgetcsv($file, 10000, ";")) !== FALSE)
+	         {
+ 
+	          //It wiil insert a row to our subject table from our csv file`
+	           $sql = "INSERT into plandeestudio (`ano`, `periodo`, `nombreSede`, `planDeEstudio`,escuela, `codJornada`, `nivel`, `seccion`, `codAsignatura`, `nombreAsignatura`, `rutDocente`, `Instructor`) 
+	            	values('$emapData[0]','$emapData[1]','$emapData[2]','$emapData[3]','$emapData[4]','$emapData[5]','$emapData[6]','$emapData[7]','$emapData[8]','$emapData[9]','$emapData[10]','$emapData[11]')";
+	         //we are using mysql_query function. it returns a resource on true else False on error
+	          $result = mysqli_query( $con, $sql );
+				if(! $result )
+				{
+					echo "<script type=\"text/javascript\">
+							alert(\"Invalid File:Please Upload CSV File.\");
+							window.location = \"PruebaSubidaExcel.php\"
+						</script>";
+ 
+				}
+ 
+	         }
+	         fclose($file);
+	         //throws a message if data successfully imported to mysql database from excel file
+	         echo "<script type=\"text/javascript\">
+						alert(\"CSV File has been successfully Imported.\");
+						window.location = \"PruebaSubidaExcel.php\"
+					</script>";
+ 
+ 
+ 
+			 //close of connection
+			mysqli_close($con); 
+ 
+ 
+ 
+		 }
+	}	 
+?>
