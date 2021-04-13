91205b2b56739a74bb8154207bd890d647979d99
felipetorresf <felipeantonio.torresfarias@gmail.com>
2021-04-13 11:20:04 -0400

    menúAdministradorV2

--- administrador.php
+++ administrador.php
@@ -3,16 +3,69 @@
 <?php $currentPage = 'Administracion'; ?>
 <?php include('head.php'); ?>
 <?php include('nav-bar.php'); ?>
-<link rel="stylesheet" href="css/menu.css" type="text/css"  />
 
 <body>
+<style>
+    *{
+  margin: 0;
+  padding: 0;
+}
 
-<nav><ul>
-        <li><a href="creacionPreguntas.php">Ingresar Preguntas</a></li>
-        <li><a href="ResultadoEncuestaG.php ">Sabana</a></li>
-        <li><a href="Reporte.php ">Todos los usuario</a></li>
-        <li><a href="signup-user.php">Registrar Usuario</a></li>
-    </ul></nav>
+ul{
+  list-style: none;
+}
+#menu li>a{
+  background-color: grey;
+  color: white;
+  padding: 10px;
+  display: block;
+  text-decoration: none;
+  min-width: 100px;
+}
+#menu li>a:hover{
+  color: #000;
+  background-color: #eaeaea;
+}
+#menu>li{
+  float: left;
+  text-align:center
+}
+#menu>li>ul{
+  display: none;
+}
+#menu>li:hover>ul {
+  display:block;
+}
+</style>
+ 
+    
+
+  <body>
+  <header class="row">
+    <nav class="col-12 text-white py-3" >
+        <ul id="menu">
+        <li><a href="ResultadoEncuestaG.php ">Reporte</a></li>
+          <li><a href="">Tutoriales</a></li>
+          <li><a href="">Usuarios</a>
+            <ul>
+                <li><a href="Reporte.php ">Todos los usuario</a></li>
+                <li><a href="signup-user.php">Registrar Usuario</a></li>
+                <li><a href="">Eliminar Usuarios</a></li>
+              
+            </ul>  
+          </li>          
+          <li><a href="">Encuesta</a>
+          <ul>
+          <li><a href="creacionPreguntas.php">Ingresar Preguntas</a></li>
+              <li><a href="">Eliminar Preguntas</a></li>
+              <li><a href=""> Pregutas Registradas</a></li>
+            </ul> 
+          </li>
+          
+        </ul>
+      </nav>
+    </header>
+
 
 <?php
 error_reporting(E_ALL ^ E_NOTICE);
@@ -23,3 +76,13 @@ error_reporting(E_ALL ^ E_NOTICE);
 ?>
    
 </html> 
+
+
+
+<?php
+/*<nav><ul>
+        
+        
+    </ul></nav> */
+
+?>
