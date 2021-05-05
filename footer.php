ed3a0d9d4abe53f17cc6413ae31cb6477d10bc7f
fisalazarm <fabian.salazar.mallea@gmail.com>
2021-05-05 16:55:11 -0400

    footer

---
+++ footer.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <footer class="bg-light text-center text-lg-start fixed-bottom ">
+        <!-- Grid container -->
+        <div class="container p-4"></div>
+        <!-- Grid container -->
+        <!-- Copyright -->
+        <div class="text-center p-3" style="background-color: #00162b;">
+            <a class="text-white">Desarrollado por Felipe Torres y Fabián Salazar</a>
+        </div>
+        <!-- Copyright -->
+    </footer>
+</html>
